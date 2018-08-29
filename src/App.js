56c51ce4fcfeae26e7d3ed16fda9a8d2520d9acb
spences10 <spences10apps@gmail.com>
2018-08-29 10:15:50 +0100

    wrap components in router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import { MemoryRouter } from 'react-router';
+import { Components as PCFComponents } from 'platform-common-frontend';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MemoryRouter initialEntries={['/']}>
+        <PCFComponents.GovUkAppWrapper>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to
+              reload.
+            </p>
+          </div>
+        </PCFComponents.GovUkAppWrapper>
+      </MemoryRouter>
     );
   }
 }
